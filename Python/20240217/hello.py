5972f974a89490f68e828fac4a5a2836556f189a
shilinnull <shilinnull@163.com>
2024-02-23 20:43:36 +0800

    python变量练习

--- Python/20240217/hello.py
+++ Python/20240217/hello.py
@@ -1,7 +1,20 @@
 
-print("hello")
-print(1+2+3+4)
+# print("hello")
+# print(1+2+3+4)
 
-print(2/3)
+# print(2/3)
 
-print( (67.5 + 89.0 + 12.9 + 32.2) / 4 )
+# print( (67.5 + 89.0 + 12.9 + 32.2) / 4 )
+#
+#
+# avg = (67.5 + 89+12.9+32.2) / 4
+#
+# # ** 代表平方
+# total = (67.5-avg) ** 2 + (89-avg) ** 4
+#
+# print(total / 3 - total + avg ** 4)
+
+avg = (67.5 + 89.0 + 12.9 + 32.2) / 4
+total = (67.5 - avg) ** 2 + (89.0 - avg) ** 2 + (12.9 - avg) ** 2 + (32.2 - avg) ** 2
+result = total / 3
+print(result)
