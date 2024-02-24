91bcea1240be8ead7c919d2176335476e6fe3551
shilinnull <shilinnull@163.com>
2024-02-24 21:44:19 +0800

    变量练习

--- Python/learning_01/hello.py
+++ Python/learning_01/hello.py
@@ -17,4 +17,9 @@
 avg = (67.5 + 89.0 + 12.9 + 32.2) / 4
 total = (67.5 - avg) ** 2 + (89.0 - avg) ** 2 + (12.9 - avg) ** 2 + (32.2 - avg) ** 2
 result = total / 3
+
 print(result)
+
+print("hehe")
+
+
