28b1dd0dae28363f421b5ef4f62d7fe3240bfe85
shilinnull <shilinnull@163.com>
2024-02-25 17:26:13 +0800

    表达式

--- Python/learning_01/hello.py
+++ Python/learning_01/hello.py
@@ -14,12 +14,77 @@
 #
 # print(total / 3 - total + avg ** 4)
 
-avg = (67.5 + 89.0 + 12.9 + 32.2) / 4
-total = (67.5 - avg) ** 2 + (89.0 - avg) ** 2 + (12.9 - avg) ** 2 + (32.2 - avg) ** 2
-result = total / 3
+# avg = (67.5 + 89.0 + 12.9 + 32.2) / 4
+# total = (67.5 - avg) ** 2 + (89.0 - avg) ** 2 + (12.9 - avg) ** 2 + (32.2 - avg) ** 2
+# result = total / 3
+#
+# print(result)
+#
+# print("hehe")
 
-print(result)
+# a = 1000000000000
+# print(type(a))
+# print(a)
 
-print("hehe")
+# a = 'aas'
+# b = "abc"
+# c = "abc 'zhangsan' end"
+# c = 'abc "zhangsan" end'
+# d = '''abc zhhshsangsan" end'''
+# print(type(a))
+# print(a+b)
+# print(c)
+# print(d)
+#
+# print(len(d))
+#
+# f = a + b
+# print(f)
+
+# c1 = True
+# c2 = False
+# print(type(c1))
+# print(type(c2))
+
+# print(len(c1))
+
+'''
+a:int = 10
+b:str = "abc"
+c:str = 10
+print(c)
+'''
+
+"""
+这是一段文档注释
+"""
+
+# a = 10
+# print(f"a = {a + 10}")
+
+# num = input('请输入：')
+# print(f"您输入的数字是：{num}")
+# print(type(num))
+
+# num = input('请输入：')
+# num1 = input('请输入：')
+# a = int(num1) + int(num)
+# print(f'a + b = {int(num) + int(num1)}')
+# print(f'a + b = {a}')
+
+# 输入4个输，输出平均值
+
+a = input('请输入第一个数字：')
+b = input('请输入第二个数字：')
+c = input('请输入第三个数字：')
+d = input('请输入第四个数字：')
+avg = float(a) + float(b) + float(c) + float(d)
+print(f'平均值是：{avg / 4}')
+
+
+
+
+
+
 
 
