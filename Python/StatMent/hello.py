f68fe57bf1e7f06fc4f4def8e9f0a686f4ca87f8
shilinnull <shilinnull@163.com>
2024-02-28 19:45:59 +0800

    条件语句

---
+++ Python/StatMent/hello.py
@@ -0,0 +1,29 @@
+# while True:
+#     choice = input("输入：")
+#     if choice == '1':
+#         print("1")
+#     elif choice == '2':
+#         print("2")
+#     else:
+#         print("0")
+
+# a = int(input("输入："))
+# if a % 2 == 1:
+#     print("1")
+# else:
+#     print("0")
+#
+
+# a = int(input("输入："))
+# if(a == 1):
+#     print("hello")
+
+# pass空语句
+# a = int(input("输入："))
+# if a != 1:
+#     pass
+# else:
+#     print("hello")
+
+
+
