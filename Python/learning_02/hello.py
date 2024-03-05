e4bb2cede0da61dbdde37363e6cb0dff7d2dbf73
shilinnull <shilinnull@163.com>
2024-03-05 15:30:12 +0800

    函数返回值

--- Python/learning_02/hello.py
+++ Python/learning_02/hello.py
@@ -16,13 +16,25 @@
 #
 # 定义一个求和函数
 
-def calcSum(beg,end):
-    theSum = 0
-    for i in range(beg,end + 1):
-        theSum += i
-    print(theSum)
+# def calcSum(beg,end):
+#     theSum = 0
+#     for i in range(beg,end + 1):
+#         theSum += i
+#     print(theSum)
+#
+# # 调用函数
+# calcSum(1,100)
+# calcSum(300,400)
+# calcSum(1,1000)
 
-# 调用函数
-calcSum(1,100)
-calcSum(300,400)
-calcSum(1,1000)
+
+def getPoint():
+    x = 10
+    y = 20
+    return x,y
+
+
+a,b = getPoint()
+# 不要x，只要y
+_,b = getPoint()
+
