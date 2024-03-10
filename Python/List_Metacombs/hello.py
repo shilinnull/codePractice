23c9b6a81a5221189256d8dd01c5208b3b952c92
shilinnull <shilinnull@163.com>
2024-03-10 18:00:02 +0800

    列表元组

---
+++ Python/List_Metacombs/hello.py
@@ -0,0 +1,26 @@
+
+# a = [1, 2, 3, 4]
+# print(type(a))
+# print(a)
+#
+# b = list()
+# print(type(b))
+
+
+# a = [1, 'hello', True, [4, 5, 6]]
+# print(a)
+#
+# a[0] = 100
+# # a[100] = 100
+# print(a[2])
+# print(a[0])
+# print(a[3])
+#
+# print(len(a))
+#
+#
+# print(a[-2])
+# print(a[-0])
+# print(a[-3])
+
+
