cde64816f07bfd577ad653ffef95663da77c6088
shilinnull <shilinnull@163.com>
2024-03-10 21:27:19 +0800

    列表end...

--- Python/List_Metacombs/hello.py
+++ Python/List_Metacombs/hello.py
@@ -24,3 +24,83 @@
 # print(a[-3])
 
 
+# a = [1, 2, 3, 4]
+# print(a[1:4])
+
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# print(a[1:])
+# print(a[:5])
+# print(a[:-1])
+# print(a[:])
+# print(a[1::2])
+# print(a[1:-1:2])
+
+
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# print(a[::-1])
+# print(a[-1::-2])
+# print(a[-1000:100])
+
+
+# 列表遍历
+# a = [1, 2, 3, 4, 5, 6, 7]
+# for elem in a:
+#     elem += 10
+#     print(elem)
+# print(a)
+#
+# a = [1, 2, 3, 4, 5, 6, 7]
+# for i in range(0,len(a)):
+#     a[i] += 10
+#     print(a[i])
+# print(a)
+
+# a = [1, 2, 3, 4, 5, 6, 7]
+# a.append('hello')
+# print(a)
+#
+# a = [1, 2, 3, 4, 5, 6, 7]
+# a.insert(2,'world')
+# a.insert(100,'world')
+# print(a)
+#
+
+# a = [1, 2, 3, 4, 5, 6]
+# print(2 in a)
+# print(6 in a)
+# print(10 in a)
+# print(a.index(5))
+
+# 抛出异常
+# print(a.index(10))
+
+
+
+# a = [1, 2, 3, 4, 5, 6]
+# a.pop()
+# a.pop(3)
+# a.remove(5)
+# print(a)
+
+# a = [1, 2, 3, 4, 'abc', 'def']
+# a.remove('abc')
+# # a.remove('aaaa')
+# print(a)
+
+a = [1, 2, 3, 4]
+b = [5, 6, 7, 8]
+# c = a + b
+# c = b + a
+# print(c)
+# a. extend(b)
+# print(a)
+# print(b)
+#
+# a += b
+# print(a)
+# print(b)
+
+# c = a.index(3)
+# a.extend(b)
+# print(a)
+
