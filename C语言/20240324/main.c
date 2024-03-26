8d31fd14f9970ddd003e4af74ddbf44ffbb4939d
shilinnull <shilinnull@163.com>
2024-03-26 11:15:22 +0800

    其他练习

---
+++ C语言/20240324/main.c
@@ -0,0 +1,108 @@
+#include<stdio.h>
+
+//void bubble(int a[],int n){
+//    for(int i = 0;i<n - 1;i++){
+//        for(int j = 0;j < n - i - 1;j++){
+//            if(a[j] < a[j + 1])
+//            {
+//                int tmp = a[j + 1];
+//                a[j + 1] = a[j];
+//                a[j] = tmp;
+//            }
+//        }
+//    }
+//}
+
+//void find_two_largest(int a[],int n,int* x,int* y){
+//    // x 为第一大 y为第二大
+//    *x = 0;
+//    *y = 0;
+//    for(int i = 0; i < n; i++){
+//       if(a[i] > *x)
+//       {
+//           *x = a[i];
+//       }
+//    }
+//    for(int i = 0;i<n;i++){
+//        if(a[i] != *x && a[i] > *y)
+//        {
+//            *y = a[i];
+//        }
+//    }
+//}
+
+//int main()
+//{
+//    char c = 0;
+//    printf("%d " ,sizeof(c));
+//    printf("%d " ,sizeof(~c));
+//    printf("%d " ,sizeof(1<< c));
+//    printf("%d " ,sizeof(c >> 10));
+//    printf("%d " ,sizeof(!c));
+//    return 0;
+//}
+
+//#include <string.h>
+//int main()
+//{
+//    int i = 1;
+//    int j = (++i) + (++i) + (++i);
+//    j = i+++10;
+//
+//    printf("%d",j);
+//    return 0;
+//}
+
+//#include<math.h>
+
+/*
+int main()
+{
+//    int i = -2.9;
+//    int j = 2.9;
+//
+//    printf("%d\n",i);
+//    printf("%d\n",j);
+
+    // 本质是向-∞取整
+//    printf("%lf\n",floor(-2.9));
+//    printf("%lf\n",floor(-2.1));
+//    printf("%lf\n",floor(2.9));
+//    printf("%lf\n",floor(2.1));
+
+    // 本质是向+∞取整
+//    printf("%lf\n",ceil(-2.9));
+//    printf("%lf\n",ceil(-2.1));
+//    printf("%lf\n",ceil(2.9));
+//    printf("%lf\n",ceil(2.1));
+
+
+    // 本质上是四舍五入
+//    printf("%lf\n",round(-2.9));
+//    printf("%lf\n",round(-2.1));
+//    printf("%lf\n",round(2.9));
+//    printf("%lf\n",round(2.1));
+
+    return 0;
+}
+*/
+
+//int main()
+//{
+//    int a = 11;
+//    int d = 7;
+////    printf("%d\n",a / d);
+////    printf("%d\n",a % d);
+////    printf("%d\n",-10 / 3);
+////    printf("%d\n",-10 / 3);
+////    char c = "h";
+////    printf("%d\n",sizeof(1));
+////    printf("%d\n",sizeof('a'));
+//    printf("%d\n",sizeof(' '));
+//    printf("%d\n",sizeof(""));
+//
+//    return 0;
+//}
+//
+
+
