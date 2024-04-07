620b7daae7892fa123f87e1a4299747c6b189d49
shilinnull <shilinnull@163.com>
2024-04-07 18:26:44 +0800

    练习

---
+++ C语言/20240407/20240407/Test.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <ctype.h>// 要注意需要包含头文件ctype.h
+#include <string.h>
+
+//int main() {
+//    char str[] = "Test String.\n";
+//    size_t i = 0;
+//
+//    while (str[i]) {
+//        if (islower(str[i])) {
+//            str[i] = toupper(str[i]);
+//        }
+//        i++;
+//    }
+//    printf("%s\n", str);
+//
+//    return 0;
+//}
+
+//int main() {
+//    char str[] = "Test String.\n";
+//    size_t i = 0;
+//
+//    while (str[i]) {
+//        strlwr();
+//        i++;
+//    }
+//    printf("%s\n", str);
+//
+//    return 0;
+//}
+
+
+
+//int main()
+//{
+//	char arr1[] = "ABCDEF";
+//	char* arr2 = strlwr(arr1,); // 返回的是转换后的字符串
+//	printf("%s", arr2);
+//
+//	return 0;
+//}
+
+int main()
+{
+	
+	return 0;
+}
