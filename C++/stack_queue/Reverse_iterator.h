4184c4bbc854e8638a1b37081f6747f98130d153
shilinnull <shilinnull@163.com>
2024-04-11 10:30:36 +0800

    反向迭代器的模拟实现

--- C++/stack_queue/Reverse_iterator.h
+++ C++/stack_queue/Reverse_iterator.h
@@ -5,6 +5,7 @@ namespace lsl
 {
 	// 适配器 -- 复用
 	template<class Iterator, class Ref, class Ptr>
+
 	struct Reverse_iterator
 	{
 		typedef template<class Iterator, class Ref, class Ptr> Self;
@@ -13,7 +14,7 @@ namespace lsl
 			:_it(it);
 		{}
 
-		Ref* operator*() {
+		Ref operator*() {
 			Iterator tmp(_it);
 			return *(--tmp);
 		}
@@ -42,7 +43,8 @@ namespace lsl
 
 	// vector和list反向迭代器实现
 	template<class T>
-	class vector {
+	class vector
+	{
 	public:
 		typedef T* iterator;
 		typedef const T* const_iterator;
@@ -50,17 +52,19 @@ namespace lsl
 		// 反向迭代器适配支持
 		typedef Reverse_iterator<iterator, T&, T*> reverse_iterator;
 		typedef Reverse_iterator<const_iterator, const T&, const T*> const_reverse_iterator;
-	private:
 
-		const_reverse_iterator rbegin() const {
+		const_reverse_iterator rbegin() const
+		{
 			return const_reverse_iterator(end());
 		}
 
-		const_reverse_iterator rend() const {
+		const_reverse_iterator rend() const
+		{
 			return const_reverse_iterator(begin());
 		}
 
-		reverse_iterator rbegin() {
+		reverse_iterator rbegin()
+		{
 			return reverse_iterator(end());
 		}
 
@@ -69,11 +73,49 @@ namespace lsl
 			return reverse_iterator(begin());
 		}
 
+		iterator begin()
+		{
+			return _start;
+		}
+
+		iterator end()
+		{
+			return _finish;
+		}
+
+		const_iterator begin() const
+		{
+			return _start;
+		}
+
+		const_iterator end() const
+		{
+			return _finish;
+		}
+	private:
+		iterator _start = nullptr;
+		iterator _finish = nullptr;
+		iterator _endofstorage = nullptr;
+	};
+
+
+	template<class T>
+	struct list_node
+	{
+		list_node<T*> _next;
+		list_node<T*> _prev;
+		T _data;
+
+		list_node(const T& x = T())
+			:_prev(nullptr)
+			, _next(nullptr)
+			_data = x
+		{}
 	};
 
 	template<class T>
-	class list {
-
+	class list
+	{
 		typedef list_node<T> Node;
 	public:
 		typedef __list_iterator<T, T&, T*> iterator;
@@ -83,16 +125,47 @@ namespace lsl
 		typedef Reverse_iterator<iterator, T&, T*> reverse_iterator;
 		typedef Reverse_iterator<const_iterator, const T&, const T*> const_reverse_iterator;
 
-	};
+		const_iterator begin() const
+		{
+			return _head->_next;
+		}
 
-	reverse_iterator rbegin()
-	{
-		return reverse_iterator(end());
+		const_iterator end() const
+		{
+			return const_iterator(_head);
+		}
+
+		iterator begin()
+		{
+			return _head->_next;
+		}
+
+		iterator end()
+		{
+			return iterator(_head);
+		}
+
+		// const r 迭代器
+		const_reverse_iterator rbegin() const
+		{
+			return const_reverse_iterator(end());
+		}
+
+		const_reverse_iterator rend() const
+		{
+			return const_reverse_iterator(begin());
+		}
+
+		reverse_iterator rbegin()
+		{
+			return reverse_iterator(end());
+		}
+
+		reverse_iterator rend()
+		{
+			return reverse_iterator(begin());
+		}
+	private:
+		Node* _head;
 	}
-
-	reverse_iterator rend()
-	{
-		return reverse_iterator(begin());
-	}
-
 }
