a4cad07280edeb98f0693a0b9c5765d645fd5379
shilinnull <shilinnull@163.com>
2024-05-25 21:14:08 +0800

    设计模式——单例模式

--- C++/特殊类的设计和类型转换/特殊类的设计和类型转换/Test.cpp
+++ C++/特殊类的设计和类型转换/特殊类的设计和类型转换/Test.cpp
@@ -138,19 +138,19 @@ using namespace std;
 
 
 
-class A
-{
-public:
-	virtual void f() {}
-	int _a = 0;
-};
-
-class B : public A
-{
-
-public:
-	int _b = 1;
-};
+//class A
+//{
+//public:
+//	virtual void f() {}
+//	int _a = 0;
+//};
+//
+//class B : public A
+//{
+//
+//public:
+//	int _b = 1;
+//};
 
 //void fun(A* pa)
 //{
@@ -162,28 +162,200 @@ public:
 //}
 
 // 解决方法：使用dynamic_cast
-void fun(A* pa)
+//void fun(A* pa)
+//{
+//	B* ptr = dynamic_cast<B*>(pa);
+//	if (ptr)
+//	{
+//		ptr->_a++;
+//		ptr->_b++;
+//	}
+//	else
+//	{
+//		cout << "转换失败" << endl;
+//	}
+//}
+
+//int main()
+//{
+//	// 向下转换规则：父类对象不能转换成子类对象，但是父类指针和引用可以转换子类指针和引用
+//	B b1;
+//	A a;
+//	B b;
+//	fun(&a);
+//	fun(&b);
+//
+//	return 0;
+//}
+
+#include<map>
+
+// 懒汉模式
+//class A
+//{
+//public:
+//	static A * GetInstance()
+//	{
+//		return &_inst;
+//	}
+//
+//	void Add(const string & key, const string & value)
+//	{
+//		_dict[key] = value;
+//	}
+//
+//	void Print()
+//	{
+//		for (auto& kv : _dict)
+//		{
+//			cout << kv.first << ":" << kv.second << endl;
+//		}
+//		cout << endl;
+//	}
+//private:
+//	// 构造函数私有
+//	A()
+//	{}
+//
+//	A(const A & aa) = delete;
+//	A& operator=(const A& aa) = delete;
+//
+//	map<string, string> _dict;
+//	int _n = 0;
+//
+//	static A _inst;
+//};
+//
+//// 在程序入口之前就完成单例对象的初始化
+//A A::_inst;
+//
+//int main()
+//{
+//	// 无法创建
+//	//A aa1;
+//	//A aa2;
+//
+//	// 可以进行调用
+//	A::GetInstance()->Add("sort", "排序");
+//	A::GetInstance()->Add("left", "左边");
+//	A::GetInstance()->Add("right", "右边");
+//	A::GetInstance()->Print();
+//
+//	// 拷贝构造禁止使用
+//	// A copy(*A::GetInstance());
+//	// copy.Print();
+//
+//	// A::GetInstance()->Add("left", "剩余");
+//	// copy.Print();
+//	// A::GetInstance()->Print();
+//
+//	// 赋值也无法使用
+//	// *A::GetInstance() = *A::GetInstance();
+//
+//	return 0;
+//}
+
+
+class B
 {
-	B* ptr = dynamic_cast<B*>(pa);
-	if (ptr)
+public:
+	static B* GetInstance()
 	{
-		ptr->_a++;
-		ptr->_b++;
+		if (_inst == nullptr)
+		{
+			_inst = new B;
+		}
+
+		return _inst;
 	}
-	else
+
+	void Add(const string& key, const string& value)
 	{
-		cout << "转换失败" << endl;
+		_dict[key] = value;
 	}
-}
+
+	void Print()
+	{
+		for (auto& kv : _dict)
+		{
+			cout << kv.first << ":" << kv.second << endl;
+		}
+		cout << endl;
+	}
+
+	static void DelInstance()
+	{
+		if (_inst)
+		{
+			delete _inst;
+			_inst = nullptr;
+		}
+	}
+
+private:
+	B()
+	{}
+
+	~B()
+	{
+		// 持久化：要求把数据写到文件
+		cout << "数据写到文件" << endl;
+	}
+
+	B(const B& aa) = delete;
+	B& operator=(const B& aa) = delete;
+
+	map<string, string> _dict;
+	int _n = 0;
+	static B* _inst;
+
+	// 期望main函数结束后自动调用
+	class gc
+	{
+	public:
+		~gc()
+		{
+			DelInstance();
+		}
+	};
+	static gc _gc;
+};
+
+B* B::_inst = nullptr;
+
+// 期望main函数结束后自动调用
+B::gc B::_gc;
 
 int main()
 {
-	// 向下转换规则：父类对象不能转换成子类对象，但是父类指针和引用可以转换子类指针和引用
-	B b1;
-	A a;
-	B b;
-	fun(&a);
-	fun(&b);
+	B::GetInstance()->Add("sort", "排序");
+	B::GetInstance()->Add("left", "左边");
+	B::GetInstance()->Add("right", "右边");
+	B::GetInstance()->Print();
+
+	B::GetInstance()->Add("right", "xxx");
+	B::GetInstance()->Print();
+	
+	// 显示的调用释放
+	B::DelInstance();
+	cout << "xxxxxxxxxxx" << endl;
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
