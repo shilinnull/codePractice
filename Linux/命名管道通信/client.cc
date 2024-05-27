dba1b0e91b662d8e29713a32192171b7563b6a11
shilinnull <shilinnull@163.com>
2024-05-27 16:27:24 +0800

    修改文件名

--- Linux/命名管道通信/client.cc
+++ Linux/命名管道通信/client.cc
@@ -1,35 +1,19 @@
-#include "Shm.hpp"
-#include "Log.hpp"
 #include "namedPipe.hpp"
 
+// 写Write
 int main()
 {
-    // 1. 创建共享内存
-    Shm shm(gpathname, gproj_id, gUser);
-    shm.Zero(); // 清空共享内存
-    char *shmaddr = (char *)shm.Addr();
-
-    // 打印信息
-    shm.DebugShm();
-
-    // 2. 打开管道
     NamePiped fifo(comm_path, User);
-    fifo.OpenForWrite();
-
-    // 当成string
-    char ch = 'A';
-    while (ch <= 'Z')
+    if (fifo.OpenForWrite())
     {
-        shmaddr[ch - 'A'] = ch;
-
-        std::string temp = "wakeup";
-        std::cout << "add " << ch << " into Shm, "
-                  << "wakeup reader" << std::endl;
-        fifo.WriteNamedPipe(temp);
-        sleep(2);
-        ch++;
+        std::cout << "client open namd pipe done" << std::endl;
+        while (true)
+        {
+            std::cout << "Please Enter> ";
+            std::string message;
+            std::getline(std::cin, message);
+            fifo.WriteNamedPipe(message);
+        }
     }
-
     return 0;
 }
-
--- Linux/命名管道通信/server.cc
+++ Linux/命名管道通信/server.cc
@@ -1,24 +1,35 @@
-#include "Shm.hpp"
-#include "Log.hpp"
 #include "namedPipe.hpp"
 
+
+// 读read
 int main()
 {
-    Shm shm(gpathname, gproj_id, gCreater);
-    char *shmaddr = (char *)shm.Addr();
-
-    // 查看信息
-    shm.DebugShm();
-
-    // 2. 创建管道
+    // 对于读端而言,如果我们打开文件，但是写还没来，我会阻塞在open调用中，直到对方打开
+    // 也就是相当于进程同步
     NamePiped fifo(comm_path, Creater);
-    fifo.OpenForRead();
-
-    while (true)
+    if (fifo.OpenForRead())
     {
-        std::string temp;
-        fifo.ReadNamedPipe(&temp);
-        std::cout << "shm memory content: " << shmaddr << std::endl;
+        std::cout << "server open named pipe done" << std::endl;
+        sleep(3);
+        while (true)
+        {
+            std::string message;
+            int n = fifo.ReadNamedPipe(&message);
+            if (n > 0)
+            {
+                std::cout << "Client Say> " << message << std::endl;
+            }
+            else if (n == 0)
+            {
+                std::cout << "Client quit, Server Too!" << std::endl;
+                break;
+            }
+            else
+            {
+                std::cout << "fifo.ReadNamedPipe Error" << std::endl;
+                break;
+            }
+        }
     }
 
     return 0;
