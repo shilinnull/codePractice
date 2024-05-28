5acd6a9c1412049000ef4be2bbe85339ee2de1f5
shilinnull <shilinnull@163.com>
2024-05-28 19:59:57 +0800

    共享内存

--- Linux/共享内存和日志/server.cc
+++ Linux/共享内存和日志/server.cc
@@ -23,3 +23,21 @@ int main()
 
     return 0;
 }
+
+/*
+int main()
+{
+    Shm shm("/root/111/code-exercise/Linux/homework/共享内存/shm.hpp", 0x666, gCreater);
+    char *shmaddr = (char *)shm.Addr();
+
+    // 创建管道
+    NamePiped fifo("./fifo", Creater);
+    fifo.OpenForRead();
+
+    std::string temp;
+    fifo.ReadNamedPipe(&temp);
+    std::cout << temp << std::endl;
+
+    return 0;
+}
+*/
