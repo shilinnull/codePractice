6290d5520e8715c87fa93d97bf583583a5e0bb2a
shilinnull <shilinnull@163.com>
2024-05-30 19:06:32 +0800

    哈希table

--- C++/homework_hash/homework_hash/Hash.h
+++ C++/homework_hash/homework_hash/Hash.h
@@ -1,7 +1,8 @@
 
+#include <iostream>
+#include <vector>
 
-
-
+using namespace std;
 
 // 注意：假如实现的哈希表中元素唯一，即key相同的元素不再进行插入
 // 为了实现简单，此哈希表中我们将比较直接与元素绑定在一起
@@ -12,43 +13,107 @@ namespace Close_Hash
 	template<class K, class V>
 	class HashTable
 	{
+	public:
 		struct Elem
 		{
 			pair<K, V> _val;
-			State _state;
+			State _state = EMPTY;
 		};
 
 	public:
-		HashTable(size_t capacity = 3)
-			: _ht(capacity), _size(0), _totalSize(0)
+		HashTable()
 		{
-			for (size_t i = 0; i < capacity; ++i)
-				_ht[i]._state = EMPTY;
+			_ht.resize(3);
 		}
 
 		// 插入
-		bool Insert(const pair<K, V>& val);
+		bool Insert(const pair<K, V>& val)
+		{
+			if (Find(val.first))
+				return false;
+
+			// 判断是否需要调整哈希表的大小
+			if (_ht.size() == 0)
+			{
+				_ht.resize(10);
+			}
+			
+			if (_n * 10 >= _ht.capacity()) // 注意这里
+			{
+				// 创建新的哈希表
+				HashTable<K, V> newHT;
+				// 2倍扩容
+				newHT._ht.resize(_ht.size() * 2);
+				// 插入
+				for (size_t i = 0; i < _ht.size(); i++)
+				{
+					if (_ht[i]._state == EXIST)
+					{
+						newHT.Insert(_ht[i]._val);
+					}
+				}
+				// 交换
+				_ht.swap(newHT._ht);
+			}
+			size_t hashi = val.first % _ht.size();
+			while (_ht[hashi]._state == EXIST)
+			{
+				++hashi;
+				hashi %= _ht.size();
+			}
+
+			// 将数据插入该位置，并将该位置的状态设置为EXIST
+			_ht[hashi]._val = val;
+			_ht[hashi]._state = EXIST;
+
+			++_n;
+			return true;
+		}
 
 		// 查找
-		size_t Find(const K& key);
+		Elem* Find(const K& key)
+		{
+			size_t hashi = key % _ht.size();
+			while (_ht[hashi]._state != EMPTY)
+			{
+				if (_ht[hashi]._state == EXIST && _ht[hashi]._val.first == key)
+				{
+					return &_ht[hashi];
+				}
+				++hashi;
+				hashi %= _ht.size();
+			}
+			return nullptr;
+		}
 
 		// 删除
-		bool Erase(const K& key);
+		bool Erase(const K& key)
+		{
+			Elem* ret = Find(key);
+			if (ret == nullptr)
+				return false;
+			else
+			{
+				ret->_state = DELETE;
+				--_n;
+
+				return true;
+			}
+		}
 
 		size_t Size()const
 		{
-			return _size;
+			return _n;
 		}
 
 		bool Empty() const
 		{
-			return _size == 0;
+			return _n == 0;
 		}
 
 		void Swap(HashTable<K, V>& ht)
 		{
-			swap(_size, ht._size);
-			swap(_totalSize, ht._totalSize);
+			swap(_n, ht._n);
 			_ht.swap(ht._ht);
 		}
 
@@ -57,11 +122,8 @@ namespace Close_Hash
 		{
 			return key % _ht.capacity();
 		}
-
-		void CheckCapacity();
 	private:
 		vector<Elem> _ht;
-		size_t _size;
-		size_t _totalSize;  // 哈希表中的所有元素：有效和已删除, 扩容时候要用到
+		size_t _n = 0;
 	};
 }
