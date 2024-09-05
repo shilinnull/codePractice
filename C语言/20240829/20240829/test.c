5aa71e525cc247cc857542de74e52afe399ba605
shilinnull <shilinnull@163.com>
2024-09-05 19:08:04 +0800

    练习

---
+++ C语言/20240829/20240829/test.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+//int main()
+//{
+//	char a = -128;
+//	//00000000000000000000000010000000
+//	//10000000
+//	//01111111
+//	//11111111111111111111111110000000
+//	printf("%u\n", a);
+//	return 0;
+//}
+
+int main()
+{
+	//char a[1000];
+	//int i;
+	//
+	//for (i = 0; i < 1000; i++)
+	//{
+	//	a[i] = -1 - i;
+
+	//}
+	//printf("%d", strlen(a));
+
+	//printf("%d", 3 % 3);
+
+	//int i, j, s;
+	//for (int i = 2; i < 4; i++)
+	//{
+	//	for(s = 0,j = 3; j < 5; j++)
+	//	{
+	//		s += i * j;
+	//	}
+	//}
+	//printf("%d\n", s);
+
+	//printf("hello\n");
+	//main();
+	/*int a[10] = {};*/
+	//int a[10] = {  };
+	//char* str;
+	//str = "hello";
+	//printf("%s", str);
+	//struct std
+	//{
+	//	int a[2];
+	//	char ch[10];
+	//	float s;
+	//	double f;
+	//}std1,*p=&std1;
+	//printf("%d", sizeof(std1));
+	//printf("%d", sizeof(p));
+	int a = 4, b = -5;
+	printf("%d,%d", ~a, ~b);
+	return 0;
+}
---
+++ C语言/题目练习/题目练习/test.c
@@ -0,0 +1,960 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+// 使用C语言写⼀个程序打印 1~100之间的奇数，要求输出的数字中间加上空格
+#if 0
+int main()
+{
+	int i = 0;
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 2 == 1)
+			printf("%d ", i);
+	}
+
+	return 0;
+}
+
+
+// 使⽤C语⾔写⼀个程序打印9*9乘法⼝诀表
+
+int main()
+{
+	for (size_t i = 1; i <= 9; i++)
+	{
+		for (size_t j = 1; j <= i; j++)
+		{
+			printf("%d*%d=%d ", j, i, i * j);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+
+// 使⽤C语⾔写⼀个程序打印100~200之间的素数，数字中间使⽤空格分割。
+#include <math.h>
+int main()
+{
+	for (size_t i = 101; i <= 200; i += 2)
+	{
+		int flag = 1;
+		for (size_t j = 2; j <= sqrt(i); j++)
+		{
+			if (i % j == 0)
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag)
+			printf("%d ", i);
+	}
+	return 0;
+}
+
+
+// 输⼊三个整数a,b,c，判断由a,b,c作为三条边组成的三⻆形，
+// 如果不能组成三⻆形则输出：⾮三⻆ 形；如果是三⻆形，再继续判断，
+// 如果是等边三⻆形，则输出：等边三⻆形；如果是等腰三⻆形，则 输出：等腰三⻆形；否则输出普通三⻆形
+
+int main()
+{
+	int a, b, c;
+	while (~scanf("%d %d %d", &a, &b, &c))
+	{
+		if (a + b < c || a + c < b || b + c < a)
+		{
+			printf("非三角形\n");
+		}
+		else
+		{
+			if (a == b && b == c)
+			{
+				printf("等边三角形\n");
+			}
+			else if ((a == b && a != c) || (b == c && a != b))
+			{
+				printf("等腰三角形\n");
+			}
+			else
+			{
+				printf("普通三角形\n");
+			}
+		}
+	}
+	return 0;
+}
+
+// 输⼊2个整数m和n，计算m和n的最⼤公约数，并打印出结果
+#include <stdbool.h>
+int main()
+{
+#if 0
+	int n, m;
+	scanf("%d %d", &m, &n);
+	int k = n < m ? m : n;
+	while (true)
+	{
+		// 每次拿k试除m和n，如果不能同时整除，则k--，继续试除
+		if (n % k == 0 && m % k == 0)
+		{
+			break;
+		}
+		k--;
+	}
+	printf("%d\n", k);
+#endif
+	int n, m;
+	scanf("%d %d", &m, &n);
+	int k = n < m ? m : n;
+	
+	while (k = m % n)
+	{
+		m = n;
+		n = k;
+	}
+	printf("%d\n",k);
+
+	return 0;
+}
+
+
+// 统计二进制1的个数
+int main()
+{
+	int n = 10;
+	int count = 0;
+	while (n)
+	{
+		n = n & (n - 1);
+		count++;
+	}
+	printf("%d\n", count);
+	return 0;
+}
+
+
+// 判断一个数是否为2的次方
+int main()
+{
+	int n = 16;
+	
+	if ((n & (n - 1)) == 0)
+	{
+		printf("yes\n");
+	}
+
+	return 0;
+}
+
+
+
+int main()
+{
+	int n = 13;
+	int m = 5; 
+
+	n |= (1 << (m - 1)); // 将第m位置为1
+	printf("%d\n", n);
+
+	n &= ~(1 << (m - 1)); // 将第m位置为0
+	printf("%d\n", n);
+
+	return 0;
+}
+
+
+
+
+
+int main()
+{
+	//int i = 10;
+
+	//i = i-- - --i * (i = -3) * i++ + ++i;
+	//printf("%d\n", i);
+	int k = 0;
+	while (k = 1)
+	{
+		k = k - 1;
+	}
+	return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+	int a, b, c;
+	a = 5;
+	c = ++a; // c = 6
+	b = ++c, c++, ++a, a++; // b = 7 c = 7 a = 6 a = 7
+	b += a++ + c; // a = 8 + 7 
+	printf("a = %d b = %d c = %d\n:", a, b, c);
+	return 0;
+}
+
+
+
+#include <stdio.h>
+
+int main()
+{
+	//printf("%5d\n", 123456);
+	char a = 'ab';
+	printf("%d", a);
+	return 0;
+}
+
+
+int main()
+{
+	double sum = 0.0;
+	for (size_t i = 1; i < 100; i++)
+	{
+		if (i % 2 == 1)
+			sum += 1.0 / i;
+		if (i % 2 == 0)
+			sum -= 1.0 / i;
+	}
+	printf("%lf", sum);
+	return 0;
+}
+
+
+
+// 冒泡排序
+int main()
+{
+	int arr[10] = { 0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (size_t i = 0; i < sz; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	for (size_t i = 0; i < sz - 1; i++)
+	{
+		for (size_t j = 0; j < j - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+	for (size_t i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+
+// 计算自幂数
+#include<math.h>
+int main()
+{
+	for (size_t i = 1; i <= 100000; i++)
+	{
+		// 计算位数
+		int count = 1;
+		int tmp = i;
+		while (tmp / 10)
+		{
+			count++;
+			tmp /= 10;
+		}
+		
+		tmp = i;
+		int sum = 0;
+		while (tmp)
+		{
+			sum += (int)pow(tmp % 10, count); // 加上每一位
+			tmp /= 10;
+		}
+
+		if (sum == i)
+			printf("%d ", i);
+	}
+
+	return 0;
+}
+#endif
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
