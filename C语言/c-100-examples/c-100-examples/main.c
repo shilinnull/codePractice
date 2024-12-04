56fe979e1b0909f4d19cf810db3856c4636f58d6
shilinnull <shilinnull@163.com>
2024-12-04 13:27:34 +0800

    代码练习

--- C语言/c-100-examples/c-100-examples/main.c
+++ C语言/c-100-examples/c-100-examples/main.c
@@ -600,9 +600,45 @@ int main() {
 	}
 	return 0;
 }
+/*
+ 24
+题目：有一分数序列：2/1，3/2，5/3，8/5，13/8，21/13...求出这个数列的前20项之和。
+*/
+
+int main() {
+	int i;
+	float sum = 0, a = 2, b = 1, t;
+	for (i = 0; i < 20; i++) {
+		sum += a / b;
+		t = a;
+		a = a + b;
+        b = t;
+	}
+
+	printf("%f", sum);
+	return 0;
+}
+
+
+/*
+ 25
+题目：求 1 + 2! + 3! + ... + 20! 的和。
+*/
+int main() {
+	int i, j;
+	unsigned long long sum = 0, ret = 1;
+	for (i = 1; i <= 20; i++) {
+		ret = 1;
+		for (j = 1; j <= i; j++) {
+			ret *= j;
+		}
+		sum += ret;
+	}
+	printf("%llu", sum);
+	return 0;
+}
 #endif
 
 /*
- 23
-
+ 26
 */
