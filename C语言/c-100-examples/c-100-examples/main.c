d540a9d72da6d8b928349c540fec880a2185aaca
shilinnull <shilinnull@163.com>
2024-12-23 19:21:20 +0800

    代码练习

--- C语言/c-100-examples/c-100-examples/main.c
+++ C语言/c-100-examples/c-100-examples/main.c
@@ -1960,34 +1960,124 @@ int main() {
     return 0;
 }
 
+
+
+//在一个整型数组中，只有一个数字出现一次，其他数组都是成对出现的，请找出那个只出现一次的数字。
+//例如：
+//数组中有：1 2 3 4 5 1 2 3 4，只有5出现一次，其他数字都出现2次，找出5
+
+
+int main() {
+	int arr[] = { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
+    int x = 0;
+    for (int i = 0;i < sizeof(arr) / sizeof(arr[0]);i++) {
+        x ^= arr[i];
+    }
+    printf("%d", x);
+	return 0;
+}
+
+
+// 不允许创建临时变量，交换两个整数的内容
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+    printf("交换前：a = %d,b = %d\n", a, b);
+    a ^= b;
+    b ^= a;
+    a ^= b;
+    printf("交换后：a = %d,b = %d\n", a, b);
+
+    return 0;
+}
+
+
+
+int NumberOf1(int n) {
+    int count = 0;
+    for (int i = 0; i < 32; i++) {
+        if ((n >> i) & 1)
+            count++;
+    }
+    printf("%d", count);
+}
+
+int main() {
+    NumberOf1(-1);
+    return 0;
+}
+
+
+// 题目：获取一个整数二进制序列中所有的偶数位和奇数位，分别打印出二进制序列
+
+
+int main() {
+    int n = 10;
+    int i;
+    // 偶数位
+    for (i = 31;i >= 1;i -= 2) {
+        printf("%d ", (n >> i) & 1);
+    }
+    printf("\n");
+    // 奇数位
+    for (i = 30;i >= 0;i -= 2) {
+        printf("%d ", (n >> i) & 1);
+    }
+    return 0;
+}
+
+
+
+// 输入两个整数，求两个整数二进制格式有多少个位不同
+
+
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int i, count = 0;
+    for (i = 0; i < 32; i++) {
+        if (((n >> i) & 1) != ((m >> i) & 1)) {
+            count++;
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
+
+/*
+思路：
+1. 先将m和n进行按位异或，此时m和n相同的二进制比特位清零，不同的二进制比特位为1
+2. 统计异或完成后结果的二进制比特位中有多少个1即可
+*/
+#include <stdio.h>
+int calc_diff_bit(int m, int n)
+{
+    int tmp = m ^ n;
+    int count = 0;
+    while (tmp)
+    {
+        tmp = tmp & (tmp - 1);
+        count++;
+    }
+    return count;
+}
+
+
+int main()
+{
+    int m, n;
+    while (scanf("%d %d", &m, &n) == 2)
+    {
+        printf("%d\n", calc_diff_bit(m, n));
+    }
+    return 0;
+}
+
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
