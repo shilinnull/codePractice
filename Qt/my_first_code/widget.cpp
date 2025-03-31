1583a186858ad7f0ca2244753003b5060e6de36d
shilin <shilinnull@163.com>
2025-03-31 10:46:41 +0800

    Qt窗口练习

--- Qt/my_first_code/widget.cpp
+++ Qt/my_first_code/widget.cpp
@@ -1,11 +1,16 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include "QLabel"
+
+
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    QLabel* lable = new QLabel(this);
+    lable->setText("hello world");
 }
 
 Widget::~Widget()
--- Qt/my_first_code/widget.h
+++ Qt/my_first_code/widget.h
@@ -11,6 +11,7 @@ class Widget : public QWidget
 {
     Q_OBJECT
 
+
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
