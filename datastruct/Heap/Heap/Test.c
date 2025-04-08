6a529005e28ffe142e8cad1c36488b76b1e3ad5a
shilin <shilinnull@163.com>
2025-04-08 13:45:34 +0800

    Heap

--- datastruct/Heap/Heap/Test.c
+++ datastruct/Heap/Heap/Test.c
@@ -5,12 +5,16 @@
 int main() {
 	Heap hp;
 	HeapInit(&hp);
-	HeapPush(&hp, 1);
+	HeapPush(&hp, 6);
+	HeapPush(&hp, 10);
 	HeapPush(&hp, 2);
-	HeapPush(&hp, 3);
+	HeapPush(&hp, 8);
 	HeapPush(&hp, 4);
-	HeapPop(&hp);
-	HeapPop(&hp);
+	while (!HeapEmpty(&hp))
+	{
+		printf("%d ", HeapTop(&hp));
+		HeapPop(&hp);
+	}
 
 	HeapDestory(&hp);
 	return 0;
