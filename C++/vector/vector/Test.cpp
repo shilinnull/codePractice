43e32d3034dc9291506b1151f4e67b821085c27f
shilin <shilinnull@163.com>
2025-04-09 15:09:40 +0800

    vector的主要功能模拟实现

--- C++/vector/vector/Test.cpp
+++ C++/vector/vector/Test.cpp
@@ -19,18 +19,20 @@ void TestVectorExpand()
 		}
 	}
 }
-
-
-int main()
-{
-	//TestVectorExpand();
+void TestVector1() {
 	lsl::vector<int> v1;
 	v1.push_back(1);
 	v1.push_back(2);
 	v1.push_back(3);
 	v1.push_back(4);
-	v1.push_back(5);
+	v1.insert(v1.begin(), 10);
+	for (const auto& e : v1)
+	{
+		cout << e << " ";
+	}
 
+	cout << endl;
+	v1.erase(v1.end() - 1);
 	for (const auto& e : v1)
 	{
 		cout << e << " ";
@@ -42,6 +44,196 @@ int main()
 		cout << v1[i] << " ";
 	}
 	cout << endl;
+}
+
+void TestVector2() {
+	lsl::vector<int> v1(3, 1);
+	for (const auto& e : v1)
+		cout << e << " ";
+	cout << endl;
+
+	lsl::vector<int> v2(v1);
+	for (const auto& e : v2)
+		cout << e << " ";
+	cout << endl;
+
+	lsl::vector<int> v3 = v2;
+	for (const auto& e : v3)
+		cout << e << " ";
+	cout << endl;
+
+	lsl::vector<int> v4 = {1,2,3,4};
+	for (const auto& e : v4)
+		cout << e << " ";
+	cout << endl;
+}
+void TestVector3() {
+	std::vector<int> v{ 1,2,3,4,5,6 };
+	auto it = v.begin();
+
+	// 将有效元素个数增加到100个，多出的位置使用8填充，操作期间底层会扩容
+	v.resize(100, 8);
+	// reserve的作用就是改变扩容大小但不改变有效元素个数，操作期间可能会引起底层容量改变
+	v.reserve(100);
+	// 插入元素期间，可能会引起扩容，而导致原空间被释放
+	v.insert(v.begin(), 0);
+	v.push_back(8);
+	// 给vector重新赋值，可能会引起底层容量改变
+	v.assign(100, 8);
+	
+	/*
+	出错原因：以上操作，都有可能会导致vector扩容，也就是说vector底层原理旧空间被释
+	放掉，而在打印时，it还使用的是释放之间的旧空间，在对it迭代器操作时，实际操作的是一块
+	已经被释放的空间，而引起代码运行时崩溃。
+	解决方式：在以上操作完成之后，如果想要继续通过迭代器操作vector中的元素，只需给
+	it重新赋值即可。
+	*/
+	
+	while (it != v.end())
+	{
+		cout << *it << " ";
+		++it;
+	}
+	cout << endl;
+}
+
+void TestVector4() {
+
+	int a[] = { 1, 2, 3, 4 };
+	std::vector<int> v(a, a + sizeof(a) / sizeof(int));
+	// 使用find查找3所在位置的iterator
+	vector<int>::iterator pos = find(v.begin(), v.end(), 3);
+	// 删除pos位置的数据，导致pos迭代器失效。
+	v.erase(pos);
+	cout << *pos << endl; // 此处会导致非法访问
+}
+
+void TestVector5() {
+	vector<int> v{ 1, 2, 3, 4 };
+	auto it = v.begin();
+	while (it != v.end())
+	{
+		if (*it % 2 == 0)
+			v.erase(it);
+		++it;
+	}
+}
+void TestVector6() {
+	vector<int> v{ 1, 2, 3, 4 };
+	auto it = v.begin();
+	while (it != v.end())
+	{
+		if (*it % 2 == 0)
+			it = v.erase(it);
+		else
+			++it;
+	}
+}
+
+
+void TestVector7() {
+	lsl::vector<string> v;
+	v.push_back("1111");
+	v.push_back("2222");
+	v.push_back("3333");
+	v.push_back("4444");
+	v.push_back("5555");
+}
+
+int main()
+{
+	//TestVectorExpand();
+	//TestVector1();
+	//TestVector2();
+	//TestVector3();
+	//TestVector4();
+	//TestVector5();
+	//TestVector6();
+	TestVector7();
+
 
 	return 0;
 }
+
+
+
+
+//int main()
+//{
+//	vector<int> v{ 1,2,3,4,5 };
+//	for (size_t i = 0; i < v.size(); ++i)
+//		cout << v[i] << " ";
+//	cout << endl;
+//	auto it = v.begin();
+//	cout << "扩容之前，vector的容量为: " << v.capacity() << endl;
+//	// 通过reserve将底层空间设置为100，目的是为了让vector的迭代器失效
+//	v.reserve(100);
+//	cout << "扩容之后，vector的容量为: " << v.capacity() << endl;
+//	// 经过上述reserve之后，it迭代器肯定会失效，在vs下程序就直接崩溃了，但是linux下不会
+//	// 虽然可能运行，但是输出的结果是不对的
+//	while (it != v.end())
+//	{
+//		cout << *it << " ";
+//		++it;
+//	}
+//	cout << endl;
+//	return 0;
+//}
+
+//int main()
+//{
+//	vector<int> v{ 1,2,3,4,5 };
+//	vector<int>::iterator it = find(v.begin(), v.end(), 3);
+//	v.erase(it);
+//	cout << *it << endl;
+//	while (it != v.end())
+//	{
+//		cout << *it << " ";
+//		++it;
+//	}
+//	cout << endl;
+//	return 0;
+//}
+
+//int main()
+//{
+//	vector<int> v{ 1,2,3,4,5 };
+//	//vector<int> v{1,2,3,4,5,6};
+//	auto it = v.begin();
+//	while (it != v.end())
+//	{
+//		if (*it % 2 == 0)
+//			v.erase(it);
+//		++it;
+//	}
+//	for (auto e : v)
+//		cout << e << " ";
+//	cout << endl;
+//	return 0;
+//}
+
+//int main() {
+//	string s("hello");
+//	auto it = s.begin();
+//	// 放开之后代码会崩溃，因为resize到20会string会进行扩容
+//	// 扩容之后，it指向之前旧空间已经被释放了，该迭代器就失效了
+//	// 后序打印时，再访问it指向的空间程序就会崩溃
+//	// s.resize(20, '!');
+//	while (it != s.end())
+//	{
+//		cout << *it;
+//		++it;
+//	}
+//	cout << endl;
+//	it = s.begin();
+//	while (it != s.end())
+//	{
+//		it = s.erase(it);
+//		// 按照下面方式写，运行时程序会崩溃，因为erase(it)之后
+//		// it位置的迭代器就失效了
+//		// s.erase(it);
+//		++it;
+//	}
+//}
+
+
--- C++/vector/vector/vector.h
+++ C++/vector/vector/vector.h
@@ -6,13 +6,17 @@
 
 using namespace std;
 
-namespace lsl 
+namespace lsl
 {
 	template<class T>
 	class vector {
+		// vector 的迭代器是原生指针
 		typedef T* iterator;
 		typedef const T* const_iterator;
 	public:
+		/// <summary>
+		/// 迭代器
+		/// </summary>
 		iterator begin() {
 			return _start;
 		}
@@ -26,61 +30,210 @@ namespace lsl
 			return _finish;
 		}
 
+		/// <summary>
+		/// 构造与析构
+		/// </summary>
 		vector()
-			:_start(nullptr), _finish(nullptr), _end_of_storage(nullptr) 
 		{}
-		~vector()
-		{
-			delete[] _start;
-			_start = _finish = _end_of_storage = nullptr;
+		vector(size_t n, const T& val = T()) {
+			resize(n, val);
 		}
 
+		/*
+		* 理论上提供了vector(size_t n, const T& value = T())之后
+		* vector(int n, const T& value = T())就不需要提供了，但是对于：
+		* vector<int> v(10, 5);
+		* 编译器在编译时，认为T已经被实例化为int，而10和5编译器会默认其为int类型
+		* 就不会走vector(size_t n, const T& value = T())这个构造方法，
+		* 最终选择的是：vector(InputIterator first, InputIterator last)
+		* 因为编译器觉得区间构造两个参数类型一致，因此编译器就会将InputIterator实例化为int
+		* 但是10和5根本不是一个区间，编译时就报错了
+		* 故需要增加该构造方法
+		*/
+		vector(int n, const T& val = T()) {
+			resize(n, val);
+		}
+
+		template<class InputIterator>
+		vector(InputIterator first, InputIterator last) {
+			while (first != last)
+			{
+				push_back(*first);
+				++first;
+			}
+		}
+
+		vector(initializer_list<T> il) {
+			reserve(il.size());
+			for (auto& e : il)
+				push_back(e);
+		}
+
+		vector(const vector<T>& v) {
+			// 先开空间
+			reserve(v.capacity());
+			for (const auto& e : v)
+				push_back(e);
+		}
+		
+		void Swap(vector<T>& v) {
+			std::swap(_start, v._start);
+			std::swap(_finish, v._finish);
+			std::swap(_start, v._start);
+		}
+
+		vector<T>& operator=(const vector<T> v) {
+			Swap(v);
+			return *this;
+		}
+
+		~vector()
+		{
+			if (_start) {
+				delete[] _start;
+				_start = _finish = _end_of_storage = nullptr;
+			}
+		}
+
+		/// <summary>
+		/// 容量相关
+		/// </summary>
 		size_t size() const {
 			return _finish - _start;
 		}
+
 		size_t capacity() const {
 			return _end_of_storage - _start;
 		}
-
-		void reserve(size_t n) 
+		
+		bool empty() const
 		{
+			return _start == _finish;
+		}
+
+		void reserve(size_t n)
+		{
+			// 检查容量是否满
 			if (_finish == _end_of_storage) {
+
 				size_t oldSize = size(); // 保存旧的数据
 				T* tmp = new T[n];
-				memcpy(tmp, _start, sizeof(T) * oldSize);
+				// 不能使用memcpy，会造成浅拷贝
+				//if(_start)
+				//	memcpy(tmp, _start, sizeof(T) * oldSize);
+				
+				if (_start)
+					for (size_t i = 0; i < oldSize; i++)
+						tmp[i] = _start[i]; // 内置类型直接赋值，自定义类型调用它的赋值重载
+
+				// 释放原来的空间
 				delete[] _start;
 
+				// 改变指针
 				_start = tmp;
 				_finish = _start + oldSize;
 				_end_of_storage = _start + n;
 			}
 		}
-		void push_back(const T& x) {
-			if (_finish == _end_of_storage)
-				reserve(capacity() == 0 ? 4 : capacity() * 2);
-			*_finish++ = x;
+		
+		void resize(size_t n, const T& val = T()) {
+			// 如果比size()小直接改变_finish
+			if (n < size())
+				_finish = _start + n;
+			else {
+				// 扩容数据
+				reserve(n);
+				// 拷贝n个val
+				while (_finish != _start + n)
+					*_finish++ = val;
+			}
 		}
 
+		/// <summary>
+		/// 增删查改
+		/// </summary>
 		T& operator[](size_t i) {
 			assert(i < size());
 			return _start[i];
 		}
+		
+		const T& operator[](size_t i) const {
+			assert(i < size());
+			return _start[i];
+		}
+		T& front()
+		{
+			return *_start;
+		}
 
+		const T& front()const
+		{
+			return *_start;
+		}
 
+		T& back()
+		{
+			return *(_finish - 1);
+		}
+
+		const T& back()const
+		{
+			return *(_finish - 1);
+		}
+
+		iterator insert(iterator pos, const T& x) {
+			assert(pos >= _start);
+			assert(pos <= _finish);
+
+			// 发生了扩容,会导致迭代器失效,需要重新更新一下pos
+			if (_finish == _end_of_storage) {
+				size_t len = pos - _start;
+				reserve(capacity() == 0 ? 4 : capacity() * 2);
+				pos = _start + len;
+			}
+
+			iterator it = _finish - 1;
+			while (it >= pos)
+			{
+				*(it + 1) = *it;
+				--it;
+			}
+
+			*pos = x;
+			++_finish;
+			return pos;
+		}
+
+		// 返回删除数据的下一个数据，解决:一边遍历一边删除的迭代器失效问题
+		iterator erase(iterator pos) {
+			assert(pos >= _start);
+			assert(pos < _finish);
+			iterator it = pos + 1;
+			while (it < _finish)
+			{
+				*(it - 1) = *it;
+				++it;
+			}
+			--_finish;
+			return pos;
+		}
+
+		void push_back(const T& x) {
+			insert(end(), x);
+		}
+		//void push_back(const T& x) {
+		//	if (_finish == _end_of_storage)
+		//		reserve(capacity() == 0 ? 4 : capacity() * 2);
+		//	*_finish++ = x;
+		//}
+
+		void pop_back() {
+			erase(end() - 1);
+		}
 
 	private:
-		iterator _start;
-		iterator _finish;
-		iterator _end_of_storage;
+		iterator _start = nullptr;
+		iterator _finish = nullptr;
+		iterator _end_of_storage = nullptr;
 	};
-
-
-
-
 }
-
-
-
-
-
-
