069bd5bf1650dbdabbfdac18888ff6895c88e0a7
shilin <shilinnull@163.com>
2025-04-10 15:24:27 +0800

    联系

--- C++/code_test/code_test2/Test.cpp
+++ C++/code_test/code_test2/Test.cpp
@@ -184,15 +184,91 @@ using namespace std;
 
 
 
-template<typename T>
-T Swap(T x,T y) {
-	return x + y;
+//template<typename T>
+//T Swap(T x,T y) {
+//	return x + y;
+//}
+//
+//
+//
+#include<iostream>
+#include <vector>
+//using namespace std;
+//int main(void)
+//{
+//	vector<int>array;
+//	array.push_back(100);
+//	array.push_back(300);
+//	array.push_back(300);
+//	array.push_back(300);
+//	array.push_back(300);
+//	array.push_back(500);
+//	vector<int>::iterator itor;
+//	for (itor = array.begin();itor != array.end();itor++)
+//	{
+//		if (*itor == 300)
+//		{
+//			itor = array.erase(itor);
+//		}
+//
+//	}
+//
+//	for (itor = array.begin();itor != array.end();itor++)
+//
+//	{
+//
+//		cout << *itor << " ";
+//
+//	}
+//
+//	return 0;
+//
+//}
+//
+//
+
+//int main()
+//
+//{
+//
+//	int ar[] = { 1,2,3,4,5,6,7,8,9,10 };
+//
+//	int n = sizeof(ar) / sizeof(int);
+//
+//	vector<int> v(ar, ar + n);
+//
+//	cout << v.size() << ":" << v.capacity() << endl; // 10:10
+//
+//	v.reserve(100);
+//
+//	v.resize(20);
+//
+//	cout << v.size() << ":" << v.capacity() << endl; // 20:100
+//
+//	v.reserve(50);
+//
+//	v.resize(5);
+//
+//	cout << v.size() << ":" << v.capacity() << endl; // 5:100
+//
+//}
+
+int main()
+{
+	int ar[] = { 1,2,3,4,0,5,6,7,8,9 };
+	int n = sizeof(ar) / sizeof(int);
+	vector<int> v(ar, ar + n);
+	vector<int>::iterator it = v.begin();
+	while (it != v.end())
+	{
+		if (*it != 0)
+			cout << *it;
+		else
+			v.erase(it);
+		it++;
+	}
+	return 0;
 }
-
-
-
-
-
 
 
 
