40fcfd8c2fc3f165313bc330a62fc749b50f6f0e
shilin <shilinnull@163.com>
2025-05-02 11:58:18 +0800

    简单的记事本

---
+++ Qt/QFile/QFile/QFile.cpp
@@ -0,0 +1,76 @@
+﻿#include "QFile.h"
+
+myQFile::myQFile(QWidget* parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+
+    this->setWindowTitle("简单的记事本");
+    
+    QMenuBar* menubar = this->menuBar();
+    // 添加菜单
+    QMenu* menu = new QMenu("文件");
+    menubar->addMenu(menu);
+
+    QAction* action1 = new QAction("打开");
+    QAction* action2 = new QAction("保存");
+    menu->addAction(action1);
+    menu->addAction(action2);
+
+    // 输入框
+    edit = new QPlainTextEdit();
+    QFont font;
+    font.setPixelSize(20);
+    edit->setFont(font);
+    
+    this->setCentralWidget(edit);
+
+    // 连接信号
+    connect(action1, &QAction::triggered, this, &myQFile::handleAction1);
+    connect(action2, &QAction::triggered, this, &myQFile::handleAction2);
+}
+
+myQFile::~myQFile()
+{}
+
+void myQFile::handleAction1()
+{
+    QString path = QFileDialog::getOpenFileName(this);
+    QStatusBar* statusBar = this->statusBar();
+    statusBar->showMessage(path);
+
+    QFile file(path);
+    bool ret = file.open(QIODevice::ReadOnly);
+    if (!ret)
+    {
+        statusBar->showMessage(path + "打开失败！");
+        return;
+    }
+    // 读取文件
+    QString text = file.readAll();
+    edit->setPlainText(text); // 设置文本
+
+    file.close();
+}
+
+void myQFile::handleAction2()
+{
+    QString path = QFileDialog::getSaveFileName(this);
+
+    QStatusBar* statusBar = this->statusBar();
+
+    QFile file(path);
+    bool ret = file.open(QFile::WriteOnly);
+    if (!ret)
+    {
+        statusBar->showMessage(path + "打开失败！");
+        return;
+    }
+
+    // 写文件
+    const QString& text = edit->toPlainText();
+    file.write(text.toUtf8());
+
+    file.close();
+}
+
---
+++ Qt/QFile/QFile/QFile.h
@@ -0,0 +1,27 @@
+﻿#pragma execution_character_set("utf-8")
+#pragma once
+#include <QtWidgets/QMainWindow>
+#include "ui_QFile.h"
+
+#include <QPlainTextEdit>
+#include <QFont>
+#include <QFileDialog>
+#include <QWheelEvent>
+
+
+class myQFile : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    myQFile(QWidget *parent = nullptr);
+    ~myQFile();
+
+    void handleAction1();
+    void handleAction2();
+
+
+private:
+    QPlainTextEdit* edit;
+    Ui::QFileClass ui;
+};
