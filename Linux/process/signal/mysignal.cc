5365f1e04c2cfde7a5085cef249750e1fbf74cac
shilinnull <shilinnull@163.com>
2025-05-27 15:34:04 +0800

    signal

---
+++ Linux/process/signal/mysignal.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <signal.h>
+#include <unistd.h>
+
+
+int main()
+{
+    // signal(SIGINT, SIG_DFL);
+    signal(SIGINT, SIG_IGN);
+
+    while (true)
+    {
+        std::cout << "I am a process, " << getpid() << std::endl;
+        sleep(1);
+    }
+
+    return 0;
+}
