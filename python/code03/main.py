40f2019b64c5ea33d0154d3b973103478c9b46e8
shilinnull <shilinnull@163.com>
2025-06-01 21:41:55 +0800

    列表

---
+++ python/code03/main.py
@@ -0,0 +1,65 @@
+alist = [1, 2, 3]
+
+# alist = list()
+print(type(alist))
+alist = [1, 'hello', 3.14]
+
+print(alist[-1])
+print(alist[2])
+print(alist)
+alist = [1, 2, 3, 4, 5]
+print(alist[1:3])
+print(alist[:3])
+print(alist[:-1])
+print(alist[:])
+print(alist[1::2])
+print(alist[::-1])
+print(alist[::-2])
+print(alist[::-3])
+
+for elem in alist:
+    print(elem)
+
+for i in range(0, len(alist), 2):
+    print(alist[i]);
+
+i = 0
+while i < len(alist):
+    print(alist[i])
+    i += 1
+
+alist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+alist.append('j');
+print(alist)
+alist.insert(1, 'j');
+print(alist)
+
+print('d' in alist);
+print('x' in alist);
+
+print(alist.index('e'));
+# print(alist.index('x'));
+
+print(alist)
+alist.pop()
+print(alist)
+alist.pop(2)
+print(alist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
