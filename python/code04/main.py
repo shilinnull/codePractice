4766f26ae0086ca895d898218be4b916cd351ad8
shilinnull <shilinnull@163.com>
2025-06-02 21:42:20 +0800

    练习

---
+++ python/code04/main.py
@@ -0,0 +1,17 @@
+
+import datetime
+
+date1 = datetime.datetime.now()
+date2 = datetime.datetime(2021, 6, 1)
+print(date1 - date2)
+
+
+# import os
+# inputPath = input('请输入待搜索路径: ')
+# pattern = input('请输入待搜索关键词: ')
+# for dirpath, dirnames, filenames in os.walk(inputPath):
+#     for f in filenames:
+#         if pattern in f:
+#             print(f'{dirpath}/{f}')
+
+
