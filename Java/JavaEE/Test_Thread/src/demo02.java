18920287ba7e7e5591402d3870a811203355c9f2
Shilin Liang <shilinnull@163.com>
2025-09-05 09:49:37 +0800

    thread

--- Java/JavaEE/Test_Thread/src/demo02.java
+++ Java/JavaEE/Test_Thread/src/demo02.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class demo02 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                System.out.println("hello thread");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            System.out.println("t1 结束");
+        });
+        t1.start();
+//        System.out.println(t1.getState());
+        t1.join(3000); // 等待3秒
+        System.out.println(t1.getState());
+        System.out.println("main 结束");
+    }
 }
