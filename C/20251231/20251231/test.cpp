78e80a5090d441148847cfb215f09bf3e8c99003
Shilin Liang <shilinnull@163.com>
2025-12-31 19:37:06 +0800

    两个线程交替打印1 - 100

--- C/20251231/20251231/test.cpp
+++ C/20251231/20251231/test.cpp
@@ -53,13 +53,54 @@ private:
 	int* _count;
 };
 
+//int main() {
+//	MySharedPtr<int> p(new int(10));
+//	MySharedPtr<int> p1 = p;
+//	MySharedPtr<int> p2(p);
+//	cout << *p << endl;
+//	cout << *p1 << endl;
+//	cout << *p2 << endl;
+//
+//	return 0;
+//}
+
+// 两个线程交替打印1 - 100
+
+
+#include <Thread>
+#include <mutex>
+
 int main() {
-	MySharedPtr<int> p(new int(10));
-	MySharedPtr<int> p1 = p;
-	MySharedPtr<int> p2(p);
-	cout << *p << endl;
-	cout << *p1 << endl;
-	cout << *p2 << endl;
+	
+	std::mutex mtx;
+	condition_variable cv;
+	bool flag = false;
+	int i = 1, n = 100;
+
+	thread t1([&] {
+		while (i < n) {
+			unique_lock<std::mutex> lock(mtx);
+			while (flag) cv.wait(lock);
+			cout << "thread1: " << i++ << endl;
+			flag = true;
+			cv.notify_one();
+		}
+		});
+
+	thread t2([&] {
+		while (i < n) {
+			unique_lock<std::mutex> lock(mtx);
+			while (!flag) cv.wait(lock);
+			cout << "thread2: " << i++ << endl;
+			flag = false;
+			cv.notify_one();
+		}
+		});
+
+	t1.join();
+	t2.join();
 
 	return 0;
 }
+
+
