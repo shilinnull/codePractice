c9aaf8fa8e3bc1d4dea432a11af7796a37b06bb5
shilinnull <shilinnull@163.com>
2026-03-31 18:52:27 +0800

    CMake练习 链接静态库和动态库

---
+++ CMake/src/demo01/first_cmake.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc,char *argv[])
+{
+    cout<<"first cmake c++"<<endl; 
+    return 0;
+}
---
+++ CMake/src/demo02/test_xlog/testxlog.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#include "xlog.h"
+
+int main(int argc,char *argv[])
+{
+    Xlog log;
+    cout << "test xlog" << endl; 
+    return 0;
+}
