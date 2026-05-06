ccef9bd9f6a71471908f4b70df736360e74b8e50
shilinnull <shilinnull@163.com>
2026-05-06 21:53:36 +0800

    cmake基础使用

--- CMake/src/demo02/xlog/xlog.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "xlog.h"
-
-using namespace std;
-Xlog::Xlog() {std::cout << "Create Xlog" << std::endl;};
-Xlog::~Xlog() {std::cout << "Destroy Xlog" << std::endl;};
--- CMake/src/demo02/xlog/xlog.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-class Xlog{
-public:
-    Xlog();
-    ~Xlog();
-};
